2b77a81418169e783d8bf595183c2fb47d3a52a0
Katherine <katherinejc103@gmail.com>
2024-03-30 16:01:35 -0400

    new updates

--- src/home/EditDeck.js
+++ src/home/EditDeck.js
@@ -1,74 +1,88 @@
-import React, {useState, useEffect} from 'react'
-import {readDeck} from ".././utils/api"
-import { Link, useHistory, useParams } from 'react-router-dom';
-
+import React, { useState, useEffect } from "react";
+import { readDeck } from ".././utils/api";
+import { Link, useHistory, useParams } from "react-router-dom";
 
 export default function EditDeck() {
-  
   const { deckId } = useParams();
   const abortController = new AbortController();
-  
-  const [formData, setFormData] = useState({
-  name: '',
-  description: '',
-});
-    
-  const [currentDeck, setCurrentDeck] = useState({})
-  
-  const history = useHistory();
-  
-  useEffect(() => {
-        async function loadDeck() {
-        //  const abortController = new AbortController();
-            try {
-                const deck = await readDeck(deckId);
-                setCurrentDeck(deck);
-                setFormData({
-                          ...formData,
-                          name: deck.name,
-                          description: deck.description,
-                        });
-            } catch (error) {
-                console.error('Error loading deck:', error);
-            }
-        }
-        loadDeck(); 
-        return () => abortController.abort();
-    }, [deckId])
 
-   function handleChange(event){
-     setFormData({
-       ...formData,
-     [event.target.name] : event.target.value
-    })
-    }
-  
-    function handleCancel(){
-       history.push(`/decks/${deckId}`);
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+  });
+
+  const [currentDeck, setCurrentDeck] = useState({});
+
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadDeck() {
+      //  const abortController = new AbortController();
+      try {
+        const deck = await readDeck(deckId);
+        setCurrentDeck(deck);
+        setFormData({
+          ...formData,
+          name: deck.name,
+          description: deck.description,
+        });
+      } catch (error) {
+        console.error("Error loading deck:", error);
       }
-  
-   function handleSubmit(event){
-      event.preventDefault()
+    }
+    loadDeck();
+    return () => abortController.abort();
+  }, [deckId]);
+
+  function handleChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
   }
-  
-  
+
+  function handleCancel() {
+    history.push(`/decks/${deckId}`);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-     <div>
+    <div>
       <ol className="breadcrumb">
-          <li className="breadcrumb-item">
-              <Link to="/">Home</Link>
-          </li>
-          <li className="breadcrumb-item active">React Router / Edit Deck</li>
-        </ol>
+        <li className="breadcrumb-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="breadcrumb-item active">React Router / Edit Deck</li>
+      </ol>
       <h4>Create Deck</h4>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name</label><br/>
-        <input type="text" id="name" name="name" value={formData.name} placeholder={currentDeck.name} onChange={handleChange}/><br/>
+        <label htmlFor="name">Name</label>
+        <br />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          placeholder={currentDeck.name}
+          onChange={handleChange}
+        />
+        <br />
         <label htmlFor="description">Description</label>
-        <textarea rows="4" cols="50" name="description" value={formData.description} placeholder={currentDeck.description} onChange={handleChange}/><br/>
+        <textarea
+          rows="4"
+          cols="50"
+          name="description"
+          value={formData.description}
+          placeholder={currentDeck.description}
+          onChange={handleChange}
+        />
+        <br />
         <button onClick={handleCancel}>Cancel</button>
         <button type="submit">Submit</button>
       </form>
-      </div>
-  )
+    </div>
+  );
 }
--- src/home/StudyDeck.js
+++ src/home/StudyDeck.js
@@ -1,43 +1,45 @@
-
 import React, { useEffect, useState } from "react";
-import {
-  NavLink,
-  useParams,
-} from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import { readDeck } from "../utils/api";
-import ViewCard from "./ViewCard"
+import ViewCard from "./ViewCard";
 
 function StudyDeck() {
-    const { deckId } = useParams();
-    const [ deck, setDeck ] = useState(""); 
-    
-    useEffect(() => {       
-        // "deckId" dependency means this runs each time a new deck is selected
-        const abortController = new AbortController();
-        readDeck(deckId, abortController.signal)   
-         // calls "readDeck" api (promise)
-            .then(setDeck)                         
-             // sets deck state to the result of api promise
-        
-        return () => abortController.abort()
-    },[deckId])
+  const { deckId } = useParams();
+  const [deck, setDeck] = useState("");
 
-    if (deck) {
-        return (
-        <div>
-            <nav aria-label="breadcrumb">
-                <ol className="breadcrumb">
-                    <li className="breadcrumb-item"><a href="/">Home</a></li>
-                    <li className="breadcrumb-item"><NavLink to={`/decks/${deckId}`}>{deck.name}</NavLink></li>
-                    <li className="breadcrumb-item active" aria-current="page">Study</li>
-                </ol>
-            </nav>
-            <h1>{deck.name}: Study</h1>
-            <ViewCard deck={deck}/>
-        </div>
-    )
-        } else {
-            return <h3>Loading...</h3>
-        }
-    }
-export default StudyDeck
+  useEffect(() => {
+    // "deckId" dependency means this runs each time a new deck is selected
+    const abortController = new AbortController();
+    readDeck(deckId, abortController.signal)
+      // calls "readDeck" api (promise)
+      .then(setDeck);
+    // sets deck state to the result of api promise
+
+    return () => abortController.abort();
+  }, [deckId]);
+
+  if (deck) {
+    return (
+      <div>
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <a href="/">Home</a>
+            </li>
+            <li className="breadcrumb-item">
+              <NavLink to={`/decks/${deckId}`}>{deck.name}</NavLink>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+              Study
+            </li>
+          </ol>
+        </nav>
+        <h1>{deck.name}: Study</h1>
+        <ViewCard deck={deck} />
+      </div>
+    );
+  } else {
+    return <h3>Loading...</h3>;
+  }
+}
+export default StudyDeck;
